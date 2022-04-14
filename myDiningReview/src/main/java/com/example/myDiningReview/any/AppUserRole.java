d896e9dc22b3556eb8a36989abdcebecf03ea3d0
Florin <f.schikarski@tu-bs.de>
2022-04-14 10:49:22 +0200

    clean_master

--- myDiningReview/src/main/java/com/example/myDiningReview/any/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.myDiningReview.any;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
--- myDiningReview/src/main/java/com/example/myDiningReview/any/EmailSender.java
+++
@@ -1,8 +0,0 @@
-package com.example.myDiningReview.any;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EmailSender {
-    public void send(String to, String email);
-}
