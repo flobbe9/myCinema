1d69e667707b49f1c85daef44b9df77fe60f3cd5
Florin <f.schikarski@tu-bs.de>
2022-10-29 00:51:23 +0200

    added MapOverview and some updates

---
+++ UsefullStuff/Test.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class Test {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        List<String> list = new LinkedList<>();
+
+        
+    }
+}
